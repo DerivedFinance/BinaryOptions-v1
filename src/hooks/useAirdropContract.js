5e36fde12dc4f7059da2eff2657e2eec29fba466
Sree Vinayak Balasubramaniam <svinayak@8kmiles.com>
2021-10-23 13:49:23 -0700

    USDx claim capability working

--- src/hooks/useAirdropContract.js
+++ src/hooks/useAirdropContract.js
@@ -3,7 +3,7 @@ import useContract from "./useContract";
 import ABI from "../contracts/airdrop.json";
 
 const useAirDropContract = () => {
-  return useContract("0x2ECF708e89C35aE7e6AB3EbF8a4C1CB60e958107", ABI, false);
+  return useContract(process.env.REACT_APP_USDX_CLAIM_ADDRESS, ABI, false);
 };
 
 export default useAirDropContract;
